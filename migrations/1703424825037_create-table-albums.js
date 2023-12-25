bbd21cb5872e1f6876781e06c99a97cc5fca1c70
tsuryanto <tsuryanto16@gmail.com>
2023-12-25 09:36:49 +0700

    migrate: create table albums

---
+++ migrations/1703424825037_create-table-albums.js
@@ -0,0 +1,32 @@
+const { ALBUMS } = require('../utils/constant/Tables');
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable(ALBUMS, {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    name: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    year: {
+      type: 'SMALLINT',
+      notNull: true,
+    },
+    created_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    updated_at: {
+      type: 'TEXT',
+      notNull: true,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable(ALBUMS);
+};
